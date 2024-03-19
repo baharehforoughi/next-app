28d50a1aa3c1e99fbaaddf6fb6c6a0628bee5fc1
baharehforoughi <bhr.foroughi@gmail.com>
2024-03-19 17:00:47 +0100

    Add ProductCard component and its styles

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddToCard from "./AddToCard";
+import styles from "./ProductCard.module.css";
+
+const ProductCard = () => {
+  return (
+    // <div className={styles.card}>
+    <div>
+      <AddToCard />
+    </div>
+  );
+};
+
+export default ProductCard;
